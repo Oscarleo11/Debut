028be6623037c6a65067d4969d80db9c767d100d
Oscar Leo <oscarfolitse@gmail.com>
2024-04-23 16:25:50 +0100

    first commit Debut

---
+++ oklm.js
@@ -0,0 +1,24 @@
+// const Accueil = document.getElementById('Accueil');
+
+
+// const tas = document.getElementByNameClass("tas");
+// const firstTas = tas[1] 
+
+
+// const div = document.getElementByTagName('div');
+// const secondDiv = div[2];
+
+// const col = document.querySelector("#myId p.article > a") 
+
+// const myId = querySelectorAll
+
+// console.log(document.getElementById)
+
+
+
+
+let    Accueil = document.getElementById('body');
+
+body.style.backgroundColor = "red";
+
+console.log(article.style.color = "red");
